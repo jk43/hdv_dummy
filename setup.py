eabcb5c949001a0d0b513cff26584b863835f9c0
Hyojun Kim <hotdev@Hyojuns-MacBook-Pro.local>
2017-05-05 09:36:19 -0400

    Setup.py has updated.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
         name = 'hdv_dummy',
         packages = ["hdv_dummy"],
-        version = '0.0.1',
+        version = '0.0.2',
         url = 'https://github.com/jk43/hdv_dummy',
         author = 'Alex Hyojun Kim',
         author_email = 'alex@hotdev.com',
