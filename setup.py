2b1b964ae0faa99617a6cf7c79c6053b66742e57
Hyojun Kim <hotdev@Hyojuns-MacBook-Pro.local>
2017-05-05 13:23:18 -0400

    Setup has changed.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup
 
 setup(
-        name = 'hdv_dummy',
-        packages = ["hdv_dummy"],
-        version = '0.0.2',
-        url = 'https://github.com/jk43/hdv_dummy',
-        author = 'Alex Hyojun Kim',
-        author_email = 'alex@hotdev.com',
-        description = 'The object returns null value without any error',
-        download_url= 'https://github.com/jk43/hdv_dummy.git',
-        license = 'BSD',
-        entry_points = {}
+        name='hdv_dummy',
+        packages=["hdv_dummy"],
+        version='0.0.2',
+        url='https://github.com/jk43/hdv_dummy',
+        download_url='https://github.com/jk43/hdv_dummy.git',
+        author='Alex Hyojun Kim',
+        author_email='alex@hotdev.com',
+        description='The object returns null value without any error',
+        license='BSD',
+        entry_points={}
       )
